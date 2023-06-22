8521203c34c2064725346540d9633f6823548414
Patricio Cordoba <patricio.cordoba@parserx.com>
2023-06-21 23:22:02 -0300

    feat: fixed timezone issue

--- src/data/cygnusDao.js
+++ src/data/cygnusDao.js
@@ -2,16 +2,27 @@ import { getConnection } from './dbConnection.js'
 import 'dotenv/config'
 
 const database = process.env.CYGNUS_DATABASE
-// const database = 'sth_default'
+
+const parseDateFromTZToUTC = (dateToParse, tz, eod) => {
+  console.log(dateToParse)
+  const dateParts = dateToParse.split('-')
+  const year = (dateParts[0])
+  const month = (dateParts[1])
+  const day = (dateParts[2])
+  const time = eod ? '23:59:59' : '00:00:00'
+  // example "2023-06-21T22:10:00-03:00"
+  const stringDate = `${year}-${month}-${day}T${time}${tz}`
+
+  return new Date(stringDate)
+}
 
 const getHistoricDataFromStation = async (stationId, fromDate, toDate, parameter) => {
   const collection = 'sth_/_' + stationId + '_AirQualityObserved'
   const conn = await getConnection()
-  const parsedFromDate = new Date(fromDate)
-  parsedFromDate.setUTCHours(0, 0, 0, 0)
-  const parsedToDate = new Date(toDate)
+  // TODO deshardcodear timezone cuando se aplique a otro país que no sea ARG
+  const parsedFromDate = parseDateFromTZToUTC(fromDate, '-03:00', false)
+  const parsedToDate = parseDateFromTZToUTC(toDate, '-03:00', true)
   // esto es por si se elige el día de hoy, poder mostrar los cambios del día
-  parsedToDate.setUTCHours(23, 59, 59, 999)
   return await conn.db(database)
     .collection(collection)
     .find({ attrName: parameter, recvTime: { $gte: parsedFromDate, $lt: parsedToDate } })
